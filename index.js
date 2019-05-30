54ab77621b8271156b06178ccd23a552307c01d4
RakelQR <raquelqrodriguez@gmail.com>
2019-05-30 16:28:41 +0200

    modified server name

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const express = require('express');
-const index = express();
+const server = express();
 const path = require('path')
 const listVeterinaries = require('./veterinary.json').results;
 const port = process.env.PORT || 3001;
 
 
-index.set('port', port);
+server.set('port', port);
 
-index.use('/', express.static(path.join(__dirname, '/build')));
+server.use('/', express.static(path.join(__dirname, '/build')));
 
 /// ruta 0: /       "Hola"
 
-index.get('/', () => {
+server.get('/', () => {
     req.send('Hola')
 })
 
 /// ruta 1: /api                devuelve "Lista de APIs"
 
-index.get('/api',(req, res) => {
+server.get('/api',(req, res) => {
     res.write("/api/veterinary          List of veterinaries\n");
     res.write("/api/veterinary/:id      Detail for veterinary\n");
     res.end()
@@ -25,18 +25,18 @@ index.get('/api',(req, res) => {
 
 /// ruta 2: /api/veterinary     devuelve "Array de 100 veterinarias (JSON)"
 
-index.get('/api/veterinary', (req,res) => {
+server.get('/api/veterinary', (req,res) => {
     res.json(listVeterinaries)
 })
 
 /// ruta 3: /api/veterinary/:id devuelve "Objeto de 1 veterinaria"
 
-index.get('/api/veterinary/:id', (req, res) => {
+server.get('/api/veterinary/:id', (req, res) => {
     res.json(listVeterinaries.find(v=> v.objectId === req.params.id))
 });
 
 
-index.listen(port, function(){
+server.listen(port, function(){
     console.log('Listening on port '+ port)
 })
 
