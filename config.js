fe5305031f43103c4b7f7f30c40499a6fbb618d2
RakelQR <raquelqrodriguez@gmail.com>
2019-06-05 16:51:59 +0200

    fix config

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const url = process.env.MONGODB_URL || 'mongodb://localhost:27017/wakyma';
+console.log(`Conecting to ${url}`);
 mongoose
     .connect(url, {useNewUrlParser: true})
     .then(() => {
